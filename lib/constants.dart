12949004a803a482d850b869f464fea49b51d477
Rewalon <rewalon@subreal-team.com>
2021-02-28 12:45:14 +0500

    file const and simple desine

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+/* default colors */
+const acBottomContainerHeight = 80.0;
+const acBGColour = Color(0xFF111328);
+const acActiveCardColor = Color(0xFF1d1e33);
+const acInactiveCardColor = Color(0xFF161A3C);
+const acBottomCantainerColor = Color(0xFFEB1555);
+//0xFFEB1555
+const acLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const acTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
